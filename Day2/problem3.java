d37e36b308c2982f6ae25ae84ece7920cd1cfe44
fillopfy <abhi456tm@gmail.com>
2020-08-04 12:15:49 +0530

    Solved two more

--- Day2/problem3.java
+++ Day2/problem3.java
@@ -1,9 +1,53 @@
 /*
 
-Problem Link:
-Time: 
-Space: 
+Problem Link:https://leetcode.com/problems/next-permutation/
+
+Time: O(n)
+Space: O(1)
 
 */
 
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-1;
+        
+        while(i>0){
+            if(nums[i]>nums[i-1]){
+                int j=nums.length-1;
+                while(j>=0){
+                    if(nums[j]>nums[i-1]){
+                        break;
+                    }
+                    j--;
+                }
+                int tem=nums[i-1];
+                if(j>=0 && nums[j]>nums[i-1]){
+                    nums[i-1]=nums[j];
+                    nums[j]=tem;
+                }else{
+                    nums[i-1]=nums[i];
+                    nums[i]=tem;
+                }
+                
+                break;
+            }
+            i--;
+        }
+
+        
+        if(i==0){
+            Arrays.sort(nums);
+        }else{
+            int i1=i, i2=nums.length-1;
+            while(i2>i1){
+                int tem=nums[i1];
+                nums[i1]=nums[i2];
+                nums[i2]=tem;
+                i1++;
+                i2--;
+            }
+        }
+    }
+}
 
--- Day2/problem5.java
+++ Day2/problem5.java
@@ -1,7 +1,61 @@
 /*
 
-Problem Link:
-Time: 
-Space: 
+Problem Link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+Time: O(n)
+Space: O(1)
 
 */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i+1]>prices[i]){
+                max+=prices[i+1]-prices[i];
+            }
+            
+        }
+        return max;
+    }
+}
+
+
+
+
+
+
+
+
+/*
+
+To reduce number of transactions, use greedy approach
+
+Greedy Method:
+
+
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int peak=prices[0], vall=prices[0];
+        int i=0;
+    
+        while(i<prices.length-1){
+            while(i<prices.length-1 && prices[i]>=prices[i+1]){
+                i++;
+            }
+            vall=prices[i];
+            while(i<prices.length-1 && prices[i]<=prices[i+1]){
+                i++;
+            }
+            
+            peak=prices[i];
+            max+=peak-vall;
+            k++;
+            
+        }
+        
+        return max;
+    }
+}
