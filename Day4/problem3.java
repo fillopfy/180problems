97443514bdefd4fc3ad0eb2e4249be0cebb20844
fillopfy <abhi456tm@gmail.com>
2020-08-12 00:12:54 +0530

    Day4

--- Day4/problem3.java
+++ Day4/problem3.java
@@ -1,8 +1,64 @@
 /*
 
-Problem Link:
-Time:
-Space:
+Problem Link:https://leetcode.com/problems/longest-consecutive-sequence/
+Time:O(nlogn)
+Space:O(1)
 
+Using sorting
 */
 
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length<1)return 0;
+        Arrays.sort(nums);
+        int count=1;int max=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])continue;
+            if(nums[i]==nums[i-1]+1)count++;
+            else{
+                count=1;
+            }
+            if(count>max)max=count;
+        }
+        
+        return max;
+    }
+}
+
+
+
+
+
+/*
+using extra space, set
+Time: O(n)
+Space: O(n)
+
+
+O(n)+O(n)=O(2n)`~ O(n)
+*/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length<=1)return nums.length;
+        
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums)set.add(num);
+        int max=1;
+        for(int num: set){
+            if(!set.contains(num-1)){
+                int curr=num;
+                int streak=1;
+                
+                while(set.contains(curr+1)){
+                    curr+=1;
+                    streak+=1;
+                }
+                max=Math.max(max,streak);
+            }
+        }
+        
+        return max;
+    }
+}
--- Day4/problem4.java
+++ Day4/problem4.java
@@ -1,7 +1,36 @@
 /*
 
-Problem Link:
-Time:
-Space:
+Problem Link:https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/
+Time:O(n)
+Space:O(n)
 
 */
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        Map<Integer,  Integer> map=new HashMap<>();
+        int len=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(arr[i]==0 && len==0){
+                len=1;
+            }
+            if(sum==0){
+                len=i+1;
+            }
+            
+            if(map.containsKey(sum)){
+                int j=map.get(sum);
+                len=Math.max(len,i-j);
+            }else{
+                map.put(sum,i);
+            }
+        }
+        
+        return len;
+    }
+}
